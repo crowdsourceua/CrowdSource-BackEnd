66b24909eeee65230f41cb25af2af91e8e4b3fd1
jdcrow <josephdavid20@gmail.com>
2014-04-06 19:41:01 -0500

    sqlTest2

--- index.php
+++ index.php
@@ -11,13 +11,25 @@
     mysql_select_db($db);
 
     //Create tables
-        mysql_query("CREATE TABLE events(
-            id INT NOT NULL AUTO_INCREMENT, 
-            PRIMARY KEY(id),
-         name VARCHAR(30) )")
-         or die(mysql_error()); 
+        //mysql_query("CREATE TABLE events(
+        //    id INT NOT NULL AUTO_INCREMENT, 
+        //    PRIMARY KEY(id),
+        // name VARCHAR(30) )")
+        // or die(mysql_error()); 
     mysql_query("INSERT INTO events
-        (name) VALUES('UA vs OleMiss') ")
+        (name) VALUES('UA vs Auburn') ")
+        or die(mysql_error());
+
+    mysql_query("INSERT INTO events
+        (name) VALUES('UA vs Arkansas') ")
+        or die(mysql_error());
+    
+    mysql_query("INSERT INTO events
+        (name) VALUES('UA vs South Carolina') ")
+        or die(mysql_error());
+    
+    mysql_query("INSERT INTO events
+        (name) VALUES('UA vs The USSR') ")
         or die(mysql_error());
 
     $result = mysql_query("SELECT * FROM events")
