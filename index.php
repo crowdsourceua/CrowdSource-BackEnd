4511430d848c75fbf884f6ad910cd3e7e8621409
jdcrow <josephdavid20@gmail.com>
2014-04-07 19:30:02 -0500

    dropping tables2

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <?php
     $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
-
     $server = $url["host"];
     $username = $url["user"];
     $password = $url["pass"];
     $db = substr($url["path"],1);
-
-    mysql_connect($server, $username, $password);
-
+    $conn = mysql_connect($server, $username, $password);
+    if(!$conn)
+    {
+        die('Could not connect: ' . mysql_error());
+    }
     mysql_select_db($db);
+    
 
-    //Create tables
-        //mysql_query("CREATE TABLE events(
-        //    id INT NOT NULL AUTO_INCREMENT, 
-        //    PRIMARY KEY(id),
-        // name VARCHAR(30) )")
-        // or die(mysql_error()); 
-    //mysql_query("INSERT INTO events
-    //    (name) VALUES('UA vs Auburn') ")
-    //    or die(mysql_error());
-
-    //$result = mysql_query("SELECT * FROM events")
-    //    or die(mysql_error());
-    //$row = mysql_fetch_array($result);
-    //echo "ID: ".$row['id'];
-    //echo "Name: ".$row['name'];
-    //echo "Placeholder"
-
+    //return a JSON object with all the rows from events
+    //$sth = mysql_query("SELECT name FROM events");
+    //$rows = array();
+    //while($r = mysql_fetch_assoc($sth)) {
+    //    $rows[] = $r;
+    //}
+    $sql = "DROP TABLE events";
+    $retval = mysql_query($sql, $conn);
+    if(! $retval )
+    {
+        die('Could not delete table: ' . mysql_error());
+    }
+    //header('Content-Type: application/json');
+    //echo json_encode($rows);
+    echo "Table deleted successfully";
+    mysql_close($conn);
 
 ?>
