a5daff788b73533c492f53cfd16ac9dee60ea932
jdcrow <josephdavid20@gmail.com>
2014-04-07 23:05:52 -0500

    user and event tables created and ready for demo; json objects returned from index.php

--- index.php
+++ index.php
@@ -11,20 +11,23 @@
     }
     mysql_select_db($db);
 
+
+
+    //JSON object for events table
     $eventData = array();
     $result = mysql_query("SELECT * FROM event")
         or die(mysql_error());
     //$row = mysql_fetch_array($result);
     while($row = mysql_fetch_assoc($result)){
-        //echo "Got row: \n";
-        //print_r($row);
         $eventData[] = $row;
     }
+    
+    //JSON object for user table
     $userData = array();
     $result2 = mysql_query("SELECT * FROM users")
         or die(mysql_error());
-    while($row2 = mysql_fetch_assoc($result)){
-        $userData[] = $row;
+    while($row2 = mysql_fetch_assoc($result2)){
+        $userData[] = $row2;
     }
     echo json_encode($eventData);
     echo json_encode($userData);
