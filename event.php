358317c657f6961e265f5b496fa0a529f5b6187c
jdcrow <josephdavid20@gmail.com>
2014-04-07 23:11:43 -0500

    separate php files for each json return

---
+++ event.php
@@ -0,0 +1,27 @@
+<?php
+    $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"],1);
+    $conn = mysql_connect($server, $username, $password);
+    if(!$conn)
+    {
+        die('Could not connect: ' . mysql_error());
+    }
+    mysql_select_db($db);
+
+
+
+    //JSON object for events table
+    $eventData = array();
+    $result = mysql_query("SELECT * FROM event")
+        or die(mysql_error());
+    //$row = mysql_fetch_array($result);
+    while($row = mysql_fetch_assoc($result)){
+        $eventData[] = $row;
+    }
+    
+    echo json_encode($eventData);
+    //mysql_close($conn);
+?>
