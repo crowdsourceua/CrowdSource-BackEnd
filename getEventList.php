eaaf89af636f9c6fb3f5e369fa8414d1d96f1b98
jdcrow <josephdavid20@gmail.com>
2014-04-07 19:27:37 -0500

    dropping tables

--- getEventList.php
+++ getEventList.php
@@ -4,19 +4,29 @@
     $username = $url["user"];
     $password = $url["pass"];
     $db = substr($url["path"],1);
-    mysql_connect($server, $username, $password);
+    $conn = mysql_connect($server, $username, $password);
+    if(!$conn)
+    {
+        die('Could not connect: ' . mysql_error());
+    }
     mysql_select_db($db);
     
 
     //return a JSON object with all the rows from events
-    $sth = mysql_query("SELECT name FROM events");
-    $rows = array();
-    while($r = mysql_fetch_assoc($sth)) {
-        $rows[] = $r;
+    //$sth = mysql_query("SELECT name FROM events");
+    //$rows = array();
+    //while($r = mysql_fetch_assoc($sth)) {
+    //    $rows[] = $r;
+    //}
+    $sql = "DROP TABLE events";
+    $retval = mysql_query($sql, $conn);
+    if(! $retval )
+    {
+        die('Could not delete table: ' . mysql_error());
     }
-
-    header('Content-Type: application/json');
-    echo json_encode($rows);
-
+    //header('Content-Type: application/json');
+    //echo json_encode($rows);
+    echo "Table deleted successfully";
+    mysql_close($conn);
 
 ?>
