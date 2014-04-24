8472f46bbb7f7b39466ffc67aed8f2fd5f30494e
jdcrow <josephdavid20@gmail.com>
2014-04-23 22:07:50 -0500

    added some php stuff6

--- verifyUser.php
+++ verifyUser.php
@@ -16,7 +16,7 @@
     //    exit;
     //}
 
-    $sql = "SELECT * FROM Logins WHERE email = '$_POST[email]' AND pass = '$_POST[pass]'";
+    $sql = "SELECT * FROM Logins WHERE email = '$_GET[email]' AND pass = '$_GET[pass]'";
     $result = mysql_query($sql, $conn) or die(mysql_error());
     $num = mysql_num_rows($result);
 
