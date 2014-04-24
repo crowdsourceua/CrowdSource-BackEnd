b48970ac35d7c1981f6766f84c118d0cd8fa5e1f
jdcrow <josephdavid20@gmail.com>
2014-04-24 05:50:59 -0500

    added some php stuff6

--- getEvent.php
+++ getEvent.php
@@ -9,7 +9,7 @@
     mysql_select_db($db);
 
     //Event details:
-    $query = "SELECT eventName, opponent, location, eventVenue, eventTime,description, date FROM Questions WHERE '$_GET[eventID]' = eventID";
+    $query = "SELECT eventName, opponent, location, eventVenue, eventTime,description, date FROM event WHERE '$_GET[eventID]' = eventID";
     
     // $retval = mysql_query($query) or trigger_error(mysql_error()." in ".$query);
     $eventstuff = array();
